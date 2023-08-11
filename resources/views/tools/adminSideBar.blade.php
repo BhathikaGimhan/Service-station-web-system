c9407e0bb2a95c186f2fb76fa39a502dbb41b242
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-08-11 22:45:31 +0530

    bugFixed

--- resources/views/tools/adminSideBar.blade.php
+++ resources/views/tools/adminSideBar.blade.php
@@ -24,7 +24,7 @@
                 </div>
             </div> --}}
             <a href="{{ url('/Booking') }}" id="Booking" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Booking</a>
-            <a href="{{ url('/Forms') }}" id="Forms" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
+            {{-- <a href="{{ url('/Forms') }}" id="Forms" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a> --}}
             {{-- <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
